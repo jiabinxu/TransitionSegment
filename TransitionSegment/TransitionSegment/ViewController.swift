a8431d2874a3defdb7961c6c5953494f307877a1
jiabinxu <284696433@qq.com>
2016-09-21 16:30:36 +0800

    测试

---
+++ TransitionSegment/TransitionSegment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TransitionSegment
+//
+//  Created by wuliupai on 16/9/21.
+//  Copyright © 2016年 wuliu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
